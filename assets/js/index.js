7a6a76c22cedef324c06822443be40c4e7798d1b
jinh_gangil <gutmate@gmail.com>
2017-02-19 15:56:32 +0900

    t

--- assets/js/index.js
+++ assets/js/index.js
@@ -39,7 +39,7 @@
         $('#search').submit(function (e) {
             e.preventDefault();
             var q = $('#searchQueryEdit').val();
-            var url = 'http://search.daum.net/search?q=' + encodeURIComponent(q + ' site:gutmate.github.io');
+            var url = 'http://search.daum.net/search?q=';
             window.open(url, '', '_blank');
         });
 
