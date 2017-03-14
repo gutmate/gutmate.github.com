278e7c096ac39f0535f677a18fc059ae461caf57
jinh_gangil <gutmate@gmail.com>
2017-03-15 00:25:42 +0900

    f

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,22 @@
-$(document).ready(function(){
-  var txtTarget = $('#cover p');
-  var txt = txtTarget.html();
-  var leng = txt.length;
-  var timer = '';
-  var cnt = 0;
+(function (window, document, $) {
+    'use strict';
 
-  function typing(){
-    txtTarget.html(txt.substring(0, cnt)+'<span class="type_bar">_</span>');
-    cnt++;
-    timer = setTimeout('typing()',100);
-    if(leng < cnt){
-      clearTimeout(timer)
-          cnt = 0;
-      }
-  }
-});
+    var txtTarget = $('#cover p');
+    var txt = txtTarget.html();
+    var leng = txt.length;
+    var timer = '';
+    var cnt = 0;
+
+    function typing(){
+      txtTarget.html(txt.substring(0, cnt)+'<span class="type_bar">_</span>');
+      cnt++;
+      timer = setTimeout('typing()',100);
+      if(leng < cnt){
+        clearTimeout(timer);
+            cnt = 0;
+        }
+    }
+
+    typing();
+
+})(window, window.document, window.jQuery);
