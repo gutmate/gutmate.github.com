54ea01f3bf7c020660e2da2ee7644987a80ea25a
jinh_gangil <gutmate@gmail.com>
2017-03-15 00:31:28 +0900

    f

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,19 @@
-(function (window, document, $) {
-    'use strict';
+var txtTarget = $('#cover p');
+var txt = txtTarget.html();
+var leng = txt.length;
+var timer = '';
+var cnt = 0;
 
-    var txtTarget = $('#cover p');
-    var txt = txtTarget.html();
-    var leng = txt.length;
-    var timer = '';
-    var cnt = 0;
-
-    $(window).load(function() {
-      typing();
+// $(window).load(function() {
+//   setTimeout(typing,1000);
+// }
+typing();
+function typing(){
+  txtTarget.html(txt.substring(0, cnt)+'<span class="type_bar">_</span>');
+  cnt++;
+  timer = setTimeout('typing()',100);
+  if(leng < cnt){
+    clearTimeout(timer);
+        cnt = 0;
     }
-    
-    function typing(){
-      txtTarget.html(txt.substring(0, cnt)+'<span class="type_bar">_</span>');
-      cnt++;
-      timer = setTimeout('typing()',100);
-      if(leng < cnt){
-        clearTimeout(timer);
-            cnt = 0;
-        }
-    }
-
-})(window, window.document, window.jQuery);
+}
