732df5673ae63bd735824174eca4413f8e3ed5c0
jinh <gutmate@gmail.com>
2017-07-26 11:10:10 +0900

    Add typed js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,37 @@
-var txtTarget = $('.main_cover');
-var txt = txtTarget.html() || '';
-var leng = txt.length || 0;
-var timer = '';
-var cnt = 0;
+// var txtTarget = $('.main_cover');
+// var txt = txtTarget.html() || '';
+// var leng = txt.length || 0;
+// var timer = '';
+// var cnt = 0;
 
-$(window).load(function() {
-  if (txtTarget) {
-    txtTarget.css('opacity',1);
-    typing();
-  }
+// $(window).load(function () {
+//     if (txtTarget) {
+//         txtTarget.css('opacity', 1);
+//         typing();
+//     }
+// });
+
+// function typing() {
+//     txtTarget.html(txt.substring(0, cnt) + '<span class="type_bar">_</span>');
+//     cnt++;
+//     timer = setTimeout('typing()', 100);
+//     if (leng < cnt) {
+//         clearTimeout(timer);
+//         cnt = 0;
+//     }
+// }
+
+// TYPED.JS
+$(document).ready(function(){
+    $("#typed").typed({
+        strings: [
+            "HTML5.", "CSS3.", "Sass.", "Bootstrap.", "JavaScript.", "jQuery.",
+            "Node.js.", "Express.", "Socket.io.", "MongoDB."
+        ],
+        typeSpeed: 120,
+        startDelay: 0,
+        backSpeed: 80,
+        backDelay: 800,
+        loop: true
+    });
 });
-function typing(){
-  txtTarget.html(txt.substring(0, cnt)+'<span class="type_bar">_</span>');
-  cnt++;
-  timer = setTimeout('typing()',100);
-  if(leng < cnt){
-    clearTimeout(timer);
-        cnt = 0;
-    }
-}
